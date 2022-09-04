1a686541e40370af6bf248efc2be640a3a5a4c7a
Thiare Saez <thiareayleen@gmail.com>
2022-09-04 02:14:59 -0300

    trae datos de la persona y usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,18 +4,38 @@ export const getAllUsers = (req, res) => {
   //const allUsers = usuarioService.getAllUsuario;
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
-    conn.query("SELECT * FROM usuarios", (err, rows) => {
-      if (err) return res.send(err);
-      res.json(rows);
-    });
+    conn.query(
+      `select u.id_usuario, u.nombre_usuario, u.password, p.nombre, p.apellido_p, p.apellido_m, 
+      p.rut, p.direccion, u.imagen, c.desc as ciudad, pa.desc as pais,
+      ru.desc as rol_usuario
+      from usuarios u 
+      inner join persona p on u.id_persona = p.id_persona
+      inner join ciudad c on c.id_ciudad = p.id_ciudad
+      inner join pais pa on pa.id_pais = c.id_pais
+      inner join rol_usuarios ru on ru.id_rol = u.id_rol
+      `,
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json(rows);
+      }
+    );
   });
 };
-export const getOneUser = (req, res) => { //trae el usuario segun su id
+export const getOneUser = (req, res) => {
+  //trae el usuario segun su id
   //const user = usuarioService.getOneUsuario(req.params.userId);
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
     conn.query(
-      "SELECT * FROM usuarios WHERE id_usuario = ?",
+      `select u.nombre_usuario, u.password, p.nombre, p.apellido_p, p.apellido_m, 
+      p.rut, p.direccion, u.imagen, c.desc as ciudad, pa.desc as pais,
+      ru.desc as rol_usuario
+      from usuarios u 
+      inner join persona p on u.id_persona = p.id_persona
+      inner join ciudad c on c.id_ciudad = p.id_ciudad
+      inner join pais pa on pa.id_pais = c.id_pais
+      inner join rol_usuarios ru on ru.id_rol = u.id_rol
+      where u.id_persona = ?`,
       [req.params.id],
       (err, rows) => {
         if (err) return res.send(err);
