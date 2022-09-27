1f3894f9c15cca31f63b658747e97eeced644e2c
luferrduc <lu.ferrando@duocuc.cl>
2022-09-27 20:20:21 -0300

    Login terminado, comparacion de pass y hassPass

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,9 +7,11 @@ export const login = async (req, res) => {
   try {
     const [rows] = await pool.query(queryConsulta, [nombre_usuario]);
     if (rows.length <= 0)
-      return res
-        .status(404)
-        .json({ message: "Usuario no encontrado" });
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    const hashPassword = rows[0].password;
+    // Comparar la contraseña que viene de front con la de la BD
+    const comparedPass = await comparar(password, hashPassword);
+    if (comparedPass) return res.json({ message: "Contraseña no coincide" });
     res.json(rows[0]);
   } catch (error) {
     res.json({ message: error.message });
